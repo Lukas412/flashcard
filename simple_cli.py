5b17aa9bf592ab36248e6b0cf2316f9dd6fc5132
lukas <lukas.weber.4.12@gmail.com>
2023-09-21 23:38:37 +0200

    adjust cli to work with new data

--- simple_cli.py
+++ simple_cli.py
@@ -1,15 +1,20 @@
 import json
 import random
 
-with open('material/german/cards.json', encoding='utf8') as file:
-    cards = json.load(file)
+with open('material/german/process_description.json', encoding='utf8') as file:
+    topic = json.load(file)
+
+topic_name, cards = topic['name'], topic['cards']
 
 while True:
     random_card = random.choice(cards)
 
     print('\n' * 40)
     question, answer = random_card['question'], random_card['answer']
-    print(f'### Question ###\n{question}')
+    print(f'### Question ###')
+    print(topic_name)
+    print()
+    print(question)
     input()
     print(f'### Answer ###\n{answer}')
     input()
