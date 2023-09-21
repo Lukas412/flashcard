2e7e2cd9cdd78370e1e669e8b6c094765f97267a
lukas <lukas.weber.4.12@gmail.com>
2023-09-22 00:29:06 +0200

    improve cli data collecting

--- simple_cli.py
+++ simple_cli.py
@@ -6,15 +6,28 @@ with open('material/german/process_description.json', encoding='utf8') as file:
 
 topic_name, cards = topic['name'], topic['cards']
 
-while True:
-    random_card = random.choice(cards)
+try:
+    while True:
+        random_card = random.choice(cards)
+        question, correct_answer = random_card['question'], random_card['answer']
 
-    print('\n' * 40)
-    question, answer = random_card['question'], random_card['answer']
-    print(f'### Question ###')
-    print(topic_name)
-    print()
-    print(question)
-    input()
-    print(f'### Answer ###\n{answer}')
-    input()
+        print('\n' * 40)
+        print(f'### Question ###')
+        print(topic_name)
+        print()
+        print(question)
+        real_answer = input()
+        print(f'### Answer ###\n{correct_answer}')
+        if not real_answer or real_answer.isspace():
+            input()
+        else:
+            correctness = input('where you right?')
+            if 'corrections' not in random_card:
+                random_card['corrections'] = []
+            random_card['corrections'].append({
+                'real': real_answer,
+                'correctness': correctness
+            })
+finally:
+    with open('material/german/process_description.json', mode='w', encoding='utf8') as file:
+        json.dump(topic, file)
