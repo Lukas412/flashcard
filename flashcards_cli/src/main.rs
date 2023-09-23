4ad5fb9470cc05d3eea9e4faaaf8d4a61fd89036
lukas <lukas.weber.4.12@gmail.com>
2023-09-23 20:35:22 +0200

    add uuid to topics

--- flashcards_cli/src/main.rs
+++ flashcards_cli/src/main.rs
@@ -1,11 +1,13 @@
 use cards_core::Topic;
-use std::fs::read_to_string;
+use std::fs;
 
 fn main() -> anyhow::Result<()> {
-    let raw_topic = read_to_string("material/german/process_description.json")?;
-    let topic: Topic = serde_json::from_str(raw_topic.as_str())?;
+    let raw_topic = fs::read_to_string("material/german/process_description.json")?;
+    let mut topic: Topic = serde_json::from_str(raw_topic.as_str())?;
+    topic.generate_uuids();
 
-    dbg!(topic);
+    let raw = serde_json::to_string_pretty(&topic)?;
+    fs::write("material/german/process_description.json", raw)?;
 
     Ok(())
 }
