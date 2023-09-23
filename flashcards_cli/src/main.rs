edba24c91e2094cd783ae72949da7b682e1bf502
lukas <lukas.weber.4.12@gmail.com>
2023-09-23 16:46:32 +0200

    add data parsing to rust cli

--- flashcards_cli/src/main.rs
+++ flashcards_cli/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use cards_core::Topic;
+use std::fs::read_to_string;
+
+fn main() -> anyhow::Result<()> {
+    let raw_topic = read_to_string("material/german/process_description.json")?;
+    let topic: Topic = serde_json::from_str(raw_topic.as_str())?;
+
+    dbg!(topic);
+
+    Ok(())
 }
