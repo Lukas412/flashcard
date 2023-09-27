a47a7d561bc1813b089f0854a1bfd31f5d166b14
Lukas412 <lukas.weber.4.12@gmail.com>
2023-09-27 19:34:11 +0200

    use german name for space key

--- simple_gui.py
+++ simple_gui.py
@@ -1,5 +1,4 @@
 import csv
-import functools
 import os.path
 import pathlib
 import pickle
@@ -61,7 +60,7 @@ class FlashCardApp(tk.Tk):
         self.text_label = tk.Label(self, text='', justify=tk.LEFT, wraplength=300, font=Theme.text_font())
         self.text_label.grid(row=10, column=1, columnspan=2, sticky='nw')
 
-        self.uncover_button = (HideAbleButton(self, text='Aufdecken <Space>', font=Theme.action_font(), border=0)
+        self.uncover_button = (HideAbleButton(self, text='Aufdecken <Lehrtaste>', font=Theme.action_font(), border=0)
                                .grid(row=20, column=1, columnspan=2, sticky='nw'))
 
         self.wrong_button = (HideAbleButton(self, text='Falsch <F>', font=Theme.action_font(), border=0)
