830b269be8c79056c194d7555960d3813990bc30
Lukas412 <lukas.weber.4.12@gmail.com>
2023-09-27 13:52:57 +0200

    use class for app

--- simple_gui.py
+++ simple_gui.py
@@ -2,42 +2,52 @@ import functools
 import tkinter as tk
 
 
-def on_key_press(root):
-    def inner(event):
+class FlashCardApp(tk.Tk):
+    class State:
+        COVERED = 'covered'
+        UNCOVERED = 'uncovered'
+
+    def __init__(self):
+        super().__init__()
+
+        self.title('FlashCards')
+
+        self.grid_rowconfigure(0, minsize=32)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, minsize=32)
+        self.grid_rowconfigure(3, pad=8)
+        self.grid_columnconfigure(4, pad=32)
+        self.grid_columnconfigure(0, minsize=48)
+        self.grid_columnconfigure(1, weight=1, pad=8)
+        self.grid_columnconfigure(2, weight=1, pad=8)
+        self.grid_columnconfigure(3, minsize=48)
+
+        self.text = tk.Label(self, text='Hier kommt die Frage hin.')
+        self.text.grid(row=1, column=1, columnspan=2)
+
+        self.uncover = HideAbleButton(self, text='Karte aufdecken')
+        self.uncover.grid(row=3, column=1, columnspan=2)
+
+        self.wrong = (HideAbleButton(self, text='Falsch')
+                      .grid(row=3, column=1)
+                      .hide())
+        self.right = (HideAbleButton(self, text='Richtig')
+                      .grid(row=3, column=2)
+                      .hide())
+
+        self.bind('<KeyPress>', self.on_key_press)
+
+        self.mainloop()
+
+    def on_key_press(self, event):
         if event.char == 'q':
-            root.quit()
-
-    return inner
-
-
-def main():
-    root = tk.Tk()
-    root.title('FlashCards')
-
-    root.grid_rowconfigure(0, minsize=32)
-    root.grid_rowconfigure(1, weight=1)
-    root.grid_rowconfigure(2, minsize=32)
-    root.grid_rowconfigure(3, pad=8)
-    root.grid_columnconfigure(4, pad=32)
-    root.grid_columnconfigure(0, minsize=48)
-    root.grid_columnconfigure(1, weight=1, pad=8)
-    root.grid_columnconfigure(2, weight=1, pad=8)
-    root.grid_columnconfigure(3, minsize=48)
-
-    root.text = tk.Label(root, text='Hier kommt die Frage hin.')
-    root.text.grid(row=1, column=1, columnspan=2)
-
-    root.uncover = HideAbleButton(root, text='Karte aufdecken')
-    root.uncover.grid(row=3, column=1, columnspan=2)
-
-    root.after(1000, lambda: root.uncover.hide())
-    root.after(2000, lambda: root.uncover.show())
-    root.after(3000, lambda: root.uncover.hide())
-    root.after(4000, lambda: root.uncover.show())
-
-    root.bind('<KeyPress>', on_key_press(root))
-
-    root.mainloop()
+            self.quit()
+        if event.char == ' ':
+            print('aufdecken')
+        if event.char == 'f':
+            print('wrong')
+        if event.char == 'j':
+            print('right')
 
 
 class HideAbleButton(tk.Button):
@@ -50,13 +60,16 @@ class HideAbleButton(tk.Button):
     def grid(self, *args, **kwargs):
         super().grid(*args, **kwargs)
         self._grid_options = args, kwargs
+        return self
 
     def hide(self):
         self.grid_forget()
+        return self
 
     def show(self):
         args, kwargs = self._grid_options
         super().grid(*args, **kwargs)
+        return self
 
 
 if __name__ == '__main__':
